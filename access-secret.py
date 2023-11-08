6d7b6b68239f427906de5ff9c09106e857114215
Brent Singleton <brent.singleton@gmail.com>
2023-11-08 15:54:46 -0800

    renamed file for appropriateness, implemented input arguments

--- access-secret.py
+++ access-secret.py
@@ -1,3 +1,4 @@
+import sys
 from google.cloud import secretmanager_v1
 
 
@@ -15,9 +16,18 @@ def sample_access_secret_version(secretRef):
     response = client.access_secret_version(request=request)
 
     # Handle the response
-    print(response)
+    return response
 
 
-sample_access_secret_version(
-    "projects/th-olr-common-devqa-3beb/secrets/det-gha-var-target-environment-dev/versions/latest"
-)
+arguments = sys.argv
+if arguments[1].find("/") > 0:
+    secretName = arguments[1]
+else:
+    secretName = (
+        "projects/th-olr-common-devqa-3beb/secrets/" + arguments[1] + "/versions/latest"
+    )
+
+print("Accessing secret: " + secretName)
+secretPayload = sample_access_secret_version(secretName)
+secretValue = secretPayload.payload.data.decode("utf-8")
+print("the secret value is: " + secretValue)
